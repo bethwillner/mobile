803b04a11394cddb88025d22f4bc09c9d67fe14e
Student <student@Mac.local>
2024-01-23 09:46:18 -0800

    Adding Roman Converter

---
+++ Roman/Roman/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace Roman;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    void Convert_Clicked(System.Object sender, System.EventArgs e)
+    {
+        int intInput;
+        //declaring input 
+        bool isNumber;
+
+        Converter converter = new Converter();
+      
+
+        isNumber = Int32.TryParse(txtInput.Text, out intInput);
+        //trying to take input as either a number or a letter/string
+
+        if(isNumber)
+            //if it is a number then number to roman numeral 
+        {
+            lblDisplay.Text = converter.NumberToRoman(intInput);
+        }
+        else
+            // if it is a roman string then roman to number
+        {
+            lblDisplay.Text = converter.RomanToNumber(txtInput.Text).ToString();
+        }
+    }
+}
+
+
