238222cbb11b8b044d3d778616982ab398b1ef98
Student <student@Mac.local>
2024-01-23 08:48:16 -0800

    adding add master pro

---
+++ HelloWorld/HelloWorld/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿namespace HelloWorld;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	void Handle_Clicked(object sender, EventArgs e)
+	{
+
+		if (lblDisplay.Text == "Hello World!")
+		{
+			lblDisplay.Text = "Have a Good Day";
+		}
+		else
+		{
+			lblDisplay.Text = "Hello World!";
+		}
+	}
+}
+
+
